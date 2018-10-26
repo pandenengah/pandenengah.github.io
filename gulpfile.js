9be20324180f1aebe19d41bf0873f6648f643d9d
Pande Nengah Purnawan <pandenengah@gmail.com>
2018-10-26 08:16:24 +0800

    A

--- gulpfile.js
+++ gulpfile.js
@@ -1,122 +1,34 @@
 var gulp = require('gulp');
+var path = require('path');
 var sass = require('gulp-sass');
-var header = require('gulp-header');
-var cleanCSS = require('gulp-clean-css');
-var rename = require("gulp-rename");
-var uglify = require('gulp-uglify');
 var autoprefixer = require('gulp-autoprefixer');
-var pkg = require('./package.json');
-var browserSync = require('browser-sync').create();
+var sourcemaps = require('gulp-sourcemaps');
+var open = require('gulp-open');
 
-// Set the banner content
-var banner = ['/*!\n',
-  ' * Start Bootstrap - <%= pkg.title %> v<%= pkg.version %> (<%= pkg.homepage %>)\n',
-  ' * Copyright 2013-' + (new Date()).getFullYear(), ' <%= pkg.author %>\n',
-  ' * Licensed under <%= pkg.license %> (https://github.com/BlackrockDigital/<%= pkg.name %>/blob/master/LICENSE)\n',
-  ' */\n',
-  '\n'
-].join('');
+var Paths = {
+  HERE: './',
+  DIST: 'dist/',
+  CSS: './assets/css/',
+  SCSS_TOOLKIT_SOURCES: './assets/scss/material-dashboard.scss',
+  SCSS: './assets/scss/**/**'
+};
 
-// Copy third party libraries from /node_modules into /vendor
-gulp.task('vendor', function() {
-
-  // Bootstrap
-  gulp.src([
-      './node_modules/bootstrap/dist/**/*',
-      '!./node_modules/bootstrap/dist/css/bootstrap-grid*',
-      '!./node_modules/bootstrap/dist/css/bootstrap-reboot*'
-    ])
-    .pipe(gulp.dest('./vendor/bootstrap'))
-
-  // Font Awesome
-  gulp.src([
-      './node_modules/@fortawesome/**/*',
-    ])
-    .pipe(gulp.dest('./vendor'))
-
-  // jQuery
-  gulp.src([
-      './node_modules/jquery/dist/*',
-      '!./node_modules/jquery/dist/core.js'
-    ])
-    .pipe(gulp.dest('./vendor/jquery'))
-
-  // jQuery Easing
-  gulp.src([
-      './node_modules/jquery.easing/*.js'
-    ])
-    .pipe(gulp.dest('./vendor/jquery-easing'))
-
+gulp.task('compile-scss', function() {
+  return gulp.src(Paths.SCSS_TOOLKIT_SOURCES)
+    .pipe(sourcemaps.init())
+    .pipe(sass().on('error', sass.logError))
+    .pipe(autoprefixer())
+    .pipe(sourcemaps.write(Paths.HERE))
+    .pipe(gulp.dest(Paths.CSS));
 });
 
-// Compile SCSS
-gulp.task('css:compile', function() {
-  return gulp.src('./scss/**/*.scss')
-    .pipe(sass.sync({
-      outputStyle: 'expanded'
-    }).on('error', sass.logError))
-    .pipe(autoprefixer({
-      browsers: ['last 2 versions'],
-      cascade: false
-    }))
-    .pipe(header(banner, {
-      pkg: pkg
-    }))
-    .pipe(gulp.dest('./css'))
+gulp.task('watch', function() {
+  gulp.watch(Paths.SCSS, ['compile-scss']);
 });
 
-// Minify CSS
-gulp.task('css:minify', ['css:compile'], function() {
-  return gulp.src([
-      './css/*.css',
-      '!./css/*.min.css'
-    ])
-    .pipe(cleanCSS())
-    .pipe(rename({
-      suffix: '.min'
-    }))
-    .pipe(gulp.dest('./css'))
-    .pipe(browserSync.stream());
+gulp.task('open', function() {
+  gulp.src('examples/dashboard.html')
+    .pipe(open());
 });
 
-// CSS
-gulp.task('css', ['css:compile', 'css:minify']);
-
-// Minify JavaScript
-gulp.task('js:minify', function() {
-  return gulp.src([
-      './js/*.js',
-      '!./js/*.min.js'
-    ])
-    .pipe(uglify())
-    .pipe(rename({
-      suffix: '.min'
-    }))
-    .pipe(header(banner, {
-      pkg: pkg
-    }))
-    .pipe(gulp.dest('./js'))
-    .pipe(browserSync.stream());
-});
-
-// JS
-gulp.task('js', ['js:minify']);
-
-// Default task
-gulp.task('default', ['css', 'js', 'vendor']);
-
-// Configure the browserSync task
-gulp.task('browserSync', function() {
-  browserSync.init({
-    server: {
-      baseDir: "./"
-    }
-  });
-});
-
-// Dev task
-gulp.task('dev', ['css', 'js', 'browserSync'], function() {
-  gulp.watch('./scss/*.scss', ['css']);
-  gulp.watch('./js/*.js', ['js']);
-  gulp.watch('./*.html', browserSync.reload);
-});
+gulp.task('open-app', ['open', 'watch']);
---
+++ index.php
@@ -0,0 +1,307 @@
+<!doctype html>
+<html lang="en">
+
+<head>
+  <meta charset="utf-8" />
+  <link rel="apple-touch-icon" sizes="76x76" href="assets/img/apple-icon.png">
+  <link rel="icon" type="image/png" href="assets/img/favicon.png">
+  <meta http-equiv="X-UA-Compatible" content="IE=edge,chrome=1" />
+  <title>
+    Material Dashboard by Creative Tim
+  </title>
+  <meta content='width=device-width, initial-scale=1.0, maximum-scale=1.0, user-scalable=0, shrink-to-fit=no' name='viewport' />
+  <!--     Fonts and icons     -->
+  <link rel="stylesheet" type="text/css" href="https://fonts.googleapis.com/css?family=Roboto:300,400,500,700|Roboto+Slab:400,700|Material+Icons" />
+  <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/font-awesome/latest/css/font-awesome.min.css">
+  <!-- CSS Files -->
+  <link href="assets/css/material-dashboard.css?v=2.1.0" rel="stylesheet" />
+</head>
+
+<body class="">
+  <div class="wrapper ">
+    
+    <div class="">
+      
+      <div class="content">
+        <div class="container-fluid">
+          <div class="row">
+            <div class="col-md-12">
+              <div class="card"></div>
+            </div>
+          </div>
+          <div class="row">
+            <div class="col-md-1"></div>
+            <div class="col-md-3">
+              <div class="card card-profile">
+                <div class="card-avatar">
+                  <a href="#pablo">
+                    <img class="img" src="assets/img/foto-notrans.png" />
+                  </a>
+                </div>
+                <div class="card-body">
+                  <h6 class="text-gray"><small>- Web, Mobile, and Game Apps Enthusiast -</small></h6>
+                  <h4 class="card-title">Pande Nengah Purnawan, S.Kom</h4>
+                  
+
+
+                      <table class="table" style="margin-bottom: -1vh">
+                        <tbody>
+                          <tr></tr>
+                          <tr>
+                            <td class="text-left">Phone</td>
+                            <td class="text-right text-info">+62 819-9904-6677</td>
+                          </tr>
+                          <tr>
+                            <td class="text-left">Email</td>
+                            <td class="text-right text-info">pandenengah@gmail.com</td>
+                          </tr>
+                          <tr>
+                            <td class="text-left">DOB</td>
+                            <td class="text-right text-info">Karangasem, 15 May 1995</td>
+                          </tr>
+                          <tr>
+                            <td class="text-left">Address</td>
+                            <td class="text-right text-info">Jl. Arjuna No.28, Sading<br>
+                            <a class="text-info" href="https://goo.gl/maps/1GG2geLfYh22" target="blank"><u>View on Google Map</u></a></td>
+                          </tr>
+                          <tr>
+                            <td colspan="2"></td>
+                          </tr>
+                        </tbody>
+                      </table>
+
+                  <a href="https://www.instagram.com/pande_nengah" target="blank" class="btn btn-just-icon btn-round btn-facebook" style="background-color: #f44336; border-color: #f44336;"><i class="fa fa-instagram"></i></a>
+                  <a href="https://www.facebook.com/pande.purnawan" target="blank" class="btn btn-just-icon btn-round btn-facebook"><i class="fa fa-facebook"></i></a>
+                  <a href="https://github.com/pandenengah" target="blank" class="btn btn-just-icon btn-round btn-facebook" style="background-color: #1e1e1e; border-color: #1e1e1e;"><i class="fa fa-github"></i></a>
+
+                </div>
+              </div>
+            </div>
+
+            <div class="col-md-7">
+
+<div class="card card-nav-tabs  ">
+    <div class="card-header card-header-info">
+        <!-- colors: "header-primary", "header-info", "header-success", "header-warning", "header-danger" -->
+        <div class="nav-tabs-navigation">
+            <div class="nav-tabs-wrapper">
+                <ul class="nav nav-tabs" data-tabs="tabs">
+                    <li class="nav-item">
+                        <a class="nav-link active" href="#nav1" data-toggle="tab"><i class="material-icons">work</i>Exp</a>
+                    </li>
+                    <li class="nav-item">
+                        <a class="nav-link" href="#nav2" data-toggle="tab"><i class="material-icons">developer_board</i>Portfolio</a>
+                    </li>
+                    <li class="nav-item">
+                        <a class="nav-link" href="#nav3" data-toggle="tab"><i class="material-icons">bookmarks</i>Skill</a>
+                    </li>
+                    <li class="nav-item">
+                        <a class="nav-link" href="#nav2" data-toggle="tab"><i class="material-icons">grade</i>Award</a>
+                    </li>
+                    <li class="nav-item">
+                        <a class="nav-link" href="#nav2" data-toggle="tab"><i class="material-icons">home</i>Edu</a>
+                    </li>
+                    <li class="nav-item">
+                        <a class="nav-link" href="#nav2" data-toggle="tab"><i class="material-icons">how_to_reg</i>Organization</a>
+                    </li>
+                </ul>
+            </div>
+        </div>
+    </div><div class="card-body ">
+        <div class="tab-content text-center">
+            <div class="tab-pane active" id="nav1">
+                
+
+
+      <ul class="timeline timeline-simple">
+        <li class="timeline-inverted">
+          <div class="timeline-badge danger">
+            <i class="material-icons">card_travel</i>
+          </div>
+          <div class="timeline-panel">
+            <div class="timeline-heading">
+              <span class="badge badge-pill badge-danger">Some title</span>
+            </div>
+            <div class="timeline-body">
+              <p>Wifey made the best Father's Day meal ever. So thankful so happy so blessed. Thank you for making my family We just had fun with the “future” theme !!! It was a fun night all together ... The always rude Kanye Show at 2am Sold Out Famous viewing @ Figueroa and 12th in downtown.</p>
+            </div>
+            <h6>
+              <i class="ti-time"></i> 11 hours ago via Twitter
+            </h6>
+          </div>
+        </li>
+        <li class="timeline-inverted">
+          <div class="timeline-badge success">
+            <i class="material-icons">extension</i>
+          </div>
+          <div class="timeline-panel">
+            <div class="timeline-heading">
+              <span class="badge badge-pill badge-success">Another One</span>
+            </div>
+            <div class="timeline-body">
+              <p>Thank God for the support of my wife and real friends. I also wanted to point out that it’s the first album to go number 1 off of streaming!!! I love you Ellen and also my number one design rule of anything I do from shoes to music to homes is that Kim has to like it....</p>
+            </div>
+          </div>
+        </li>
+        <li class="timeline-inverted">
+          <div class="timeline-badge info">
+            <i class="material-icons">fingerprint</i>
+          </div>
+          <div class="timeline-panel">
+            <div class="timeline-heading">
+              <span class="badge badge-pill badge-info">Another Title</span>
+            </div>
+            <div class="timeline-body">
+              <p>Called I Miss the Old Kanye That’s all it was Kanye And I love you like Kanye loves Kanye Famous viewing @ Figueroa and 12th in downtown LA 11:10PM</p>
+              <p>What if Kanye made a song about Kanye Royère doesn't make a Polar bear bed but the Polar bear couch is my favorite piece of furniture we own It wasn’t any Kanyes Set on his goals Kanye</p>
+              <hr>
+              <div class="dropdown pull-left">
+                <button type="button" class="btn btn-round btn-info dropdown-toggle" data-toggle="dropdown">
+                  <i class="material-icons">build</i>
+                  <span class="caret"></span>
+                </button>
+                <ul class="dropdown-menu dropdown-menu-right" role="menu">
+                  <li>
+                    <a href="#action">Action</a>
+                  </li>
+                  <li>
+                    <a href="#action">Another action</a>
+                  </li>
+                  <li>
+                    <a href="#here">Something else here</a>
+                  </li>
+                  <li class="divider"></li>
+                  <li>
+                    <a href="#link">Separated link</a>
+                  </li>
+                </ul>
+              </div>
+            </div>
+          </div>
+        </li>
+      </ul>
+
+
+
+
+
+
+            </div>
+            <div class="tab-pane" id="updates">
+                <p> I will be the leader of a company that ends up being worth billions of dollars, because I got the answers. I understand culture. I am the nucleus. I think that&#x2019;s a responsibility that I have, to push possibilities, to show people, this is the level that things could be at. I think that&#x2019;s a responsibility that I have, to push possibilities, to show people, this is the level that things could be at. </p>
+            </div>
+            <div class="tab-pane" id="history">
+                <p> I think that&#x2019;s a responsibility that I have, to push possibilities, to show people, this is the level that things could be at. I will be the leader of a company that ends up being worth billions of dollars, because I got the answers. I understand culture. I am the nucleus. I think that&#x2019;s a responsibility that I have, to push possibilities, to show people, this is the level that things could be at.</p>
+            </div>
+        </div>
+    </div>
+  </div>
+            
+              <!-- <div class="card">
+                <div class="card-header card-header-primary">
+                  <h4 class="card-title">Edit Profile</h4>
+                  <p class="card-category">Complete your profile</p>
+                </div>
+                <div class="card-body">
+                  <form>
+                    <div class="row">
+                      <div class="col-md-5">
+                        <div class="form-group">
+                          <label class="bmd-label-floating">Company (disabled)</label>
+                          <input type="text" class="form-control" disabled>
+                        </div>
+                      </div>
+                      <div class="col-md-3">
+                        <div class="form-group">
+                          <label class="bmd-label-floating">Username</label>
+                          <input type="text" class="form-control">
+                        </div>
+                      </div>
+                      <div class="col-md-4">
+                        <div class="form-group">
+                          <label class="bmd-label-floating">Email address</label>
+                          <input type="email" class="form-control">
+                        </div>
+                      </div>
+                    </div>
+                    <div class="row">
+                      <div class="col-md-6">
+                        <div class="form-group">
+                          <label class="bmd-label-floating">Fist Name</label>
+                          <input type="text" class="form-control">
+                        </div>
+                      </div>
+                      <div class="col-md-6">
+                        <div class="form-group">
+                          <label class="bmd-label-floating">Last Name</label>
+                          <input type="text" class="form-control">
+                        </div>
+                      </div>
+                    </div>
+                    <div class="row">
+                      <div class="col-md-12">
+                        <div class="form-group">
+                          <label class="bmd-label-floating">Adress</label>
+                          <input type="text" class="form-control">
+                        </div>
+                      </div>
+                    </div>
+                    <div class="row">
+                      <div class="col-md-4">
+                        <div class="form-group">
+                          <label class="bmd-label-floating">City</label>
+                          <input type="text" class="form-control">
+                        </div>
+                      </div>
+                      <div class="col-md-4">
+                        <div class="form-group">
+                          <label class="bmd-label-floating">Country</label>
+                          <input type="text" class="form-control">
+                        </div>
+                      </div>
+                      <div class="col-md-4">
+                        <div class="form-group">
+                          <label class="bmd-label-floating">Postal Code</label>
+                          <input type="text" class="form-control">
+                        </div>
+                      </div>
+                    </div>
+                    <div class="row">
+                      <div class="col-md-12">
+                        <div class="form-group">
+                          <label>About Me</label>
+                          <div class="form-group">
+                            <label class="bmd-label-floating"> Lamborghini Mercy, Your chick she so thirsty, I'm in that two seat Lambo.</label>
+                            <textarea class="form-control" rows="5"></textarea>
+                          </div>
+                        </div>
+                      </div>
+                    </div>
+                    <button type="submit" class="btn btn-primary pull-right">Update Profile</button>
+                    <div class="clearfix"></div>
+                  </form>
+                </div>
+              </div> -->
+            </div>
+            
+          </div>
+        </div>
+      </div>
+
+
+    </div>
+  </div>
+  <!--   Core JS Files   -->
+  <script src="assets/js/core/jquery.min.js" type="text/javascript"></script>
+  <script src="assets/js/core/popper.min.js" type="text/javascript"></script>
+  <script src="assets/js/core/bootstrap-material-design.min.js" type="text/javascript"></script>
+  <script src="assets/js/plugins/perfect-scrollbar.jquery.min.js"></script>
+  <!-- Chartist JS -->
+  <script src="assets/js/plugins/chartist.min.js"></script>
+  <!--  Notifications Plugin    -->
+  <script src="assets/js/plugins/bootstrap-notify.js"></script>
+  <!-- Control Center for Material Dashboard: parallax effects, scripts for the example pages etc -->
+  <script src="assets/js/material-dashboard.min.js?v=2.1.0" type="text/javascript"></script>
+</body>
+
+</html>
